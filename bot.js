606b9d5ead29a1c5d39d781f4c8954ace4d75972
progamerr47 <45202979+progamerr47@users.noreply.github.com>
2019-06-22 10:58:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,11 +7,7 @@ client.user.setActivity('Seni', { type: 'WATCHING' });
 });
 
 client.on('message', msg => {
-if(msg.author.id === client.user.id)
-{
- var embed = new Discord.RichEmbed().setDescription(msg).setColor(0x00AE86)
-    msg.edit({embed});
-}
+
 });
 
 client.login(process.env.BOT_TOKEN);

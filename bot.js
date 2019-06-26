3877c6899b515826d478c4eb1bee4e35c337d0f6
progamerr47 <45202979+progamerr47@users.noreply.github.com>
2019-06-26 08:12:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setActivity('Seni', { type: 'WATCHING' });
+client.user.setActivity('PUBG LITE', { type: 'PLAYING' });
 });
 
 client.on('message', msg => {

be7ee392fee08f2893e520a61b97105924f72b7e
progamerr47 <45202979+progamerr47@users.noreply.github.com>
2019-06-29 19:24:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setActivity('Anneni', { type: 'WATCHING' });
 });
 const prefix = "!";
 
@@ -15,10 +14,9 @@ if(msg.author.id === client.user.id)
 	  let age = args[0]; // Remember arrays are 0-based!.
   let sex = args[1];
   let location = args[2];
-	if (command === 'ping') {
+	if (command === 'Durum') {
+		client.user.setActivity('${age}', { type: '${sex}' });
 		  msg.channel.send(`I see you're a ${age}`);
-
-		msg.channel.send('Pong ${args[0]} abc');
 	} else if (command === 'beep') {
 		msg.channel.send('Boop.');
 	}

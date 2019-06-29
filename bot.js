ea4c263c7d7ed8b2b995777f1f3e28672297b169
progamerr47 <45202979+progamerr47@users.noreply.github.com>
2019-06-29 19:33:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,17 +9,7 @@ const prefix = "!";
 client.on('message', msg => {
 if(msg.author.id === client.user.id)
 {
-    const args = msg.content.slice(prefix.length).trim().split(/ +/g);
-	const command = args.shift().toLowerCase();
-	  let age = args[0]; // Remember arrays are 0-based!.
-  let sex = args[1];
-  let location = args[2];
-	if (command === 'durum') {
-		client.user.setActivity('${age}', { type: '${sex}' });
-		  msg.channel.send(`${age}`);
-	} else if (command === 'beep') {
-		msg.channel.send('Boop.');
-	}
+ 
 var embed = new Discord.RichEmbed().setDescription(msg).setColor(0xFF0000)
    msg.edit({embed});
 }

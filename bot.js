32bf16cfb591f422a31ce6d36a6186f4a68f2769
progamerr47 <45202979+progamerr47@users.noreply.github.com>
2019-06-29 19:38:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity('Anneni', { type: 'LISTENING' });
+client.user.setPresence({ game: { name: 'Anneni', type: "streaming", url: "https://www.twitch.tv/progamerr478"}}); 
 
 });
 const prefix = "!";

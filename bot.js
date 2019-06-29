58c2a91b23d812d7893bf7dde8e7eeca6acfe231
progamerr47 <45202979+progamerr47@users.noreply.github.com>
2019-06-29 18:55:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,10 +5,19 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setActivity('Anneni', { type: 'WATCHING' });
 });
+const prefix = "!";
 
 client.on('message', msg => {
 if(msg.author.id === client.user.id)
 {
+  const args = msg.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		msg.channel.send('Pong.');
+	} else if (command === 'beep') {
+		msg.channel.send('Boop.');
+	}
 var embed = new Discord.RichEmbed().setDescription(msg).setColor(0xFF0000)
    msg.edit({embed});
 }

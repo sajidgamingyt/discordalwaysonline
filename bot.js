8d3f11fb5a04f0cab8adff355ee866605cea84c3
progamerr47 <45202979+progamerr47@users.noreply.github.com>
2019-06-29 19:35:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity('Seni', { type: 'LISTENING' });
+  client.user.setActivity('Anneni', { type: 'LISTENING' });
 
 });
 const prefix = "!";

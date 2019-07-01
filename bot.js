c08c4088cb32837be62334385a86addc59eedc1e
progamerr47 <45202979+progamerr47@users.noreply.github.com>
2019-07-01 14:58:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,9 +7,39 @@ client.user.setPresence({ game: { name: 'Annen ile', type: "streaming", url: "ht
 
 });
 const prefix = "!";
-
+let afk = 0;
 client.on('message', msg => {
 
+if (afk === 0)
+{
+  if(msg.author.id === client.user.id)
+{
+if {msg === "!afk"}
+  {
+    afk = 1
+  }
+}
+  return;
+}
+  else
+  {
+      if(msg.author.id === client.user.id)
+{
+if {msg === "!afk"}
+  {
+    afk = 0
+  }
+  }
+if(msg.author.id === client.user.id)
+{
+return;
+}
+if (msg.channel.type === 'dm')
+{
+
+msg.author.send('Suanda Afkyim Daha Sonra Tekrar Yazmayı dene')
+}
+  
 });
 
 client.login(process.env.BOT_TOKEN);

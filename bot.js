a41fc34b536e466dcd2c962634fecafe35a14e96
progamerr47 <45202979+progamerr47@users.noreply.github.com>
2019-07-05 05:56:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ return;
 if (msg.channel.type === 'dm')
 {
 
-msg.author.send('Suanda Uyuyorum Daha Sonra Tekrar Yazmayı dene')
+//msg.author.send('Suanda Uyuyorum Daha Sonra Tekrar Yazmayı dene')
 }
 });
 

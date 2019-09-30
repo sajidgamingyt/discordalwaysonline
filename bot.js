5430b7ccc29bb230f1173046c72dd1d1a4d072ad
sajidgamingyt <55905102+sajidgamingyt@users.noreply.github.com>
2019-09-30 07:59:13 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,14 +6,23 @@ client.on('ready', () => {
     client.user.setStatus('available')
     client.user.setPresence({
         game: {
-            name: 'DM FOR BUY KEY',
+            name: 'discord.gg/KAVBWpt',
             type: "STREAMING",
-            url: "https://www.twitch.tv/dakshophere"
+            url: "https://www.twitch.tv/settings"
         }
     });
 });
-client.on('message', message => {
 
-});
+client.on("message", function(message) {
+	if(message.content.toLowerCase() == "!dmall") {
+		console.log("starting")
+		client.guilds.get(message.guild.id).members.forEach(member1 => {
+			console.log(`Attempting to DM through selfbot API`)
+			console.log(`DM'ed ${member1.user.tag}`)
+			member1.send("Join Now :https://discord.gg/KAVBWpt")
+	
+		});
+	}
+	});
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.token);
